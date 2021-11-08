1f2f7c015457e611883bd0db86eafdd9cf87c571
kelcheone <kelcheone@gmail.com>
2021-11-08 19:18:50 +0300

    fix[import head]

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
-import Document, { Html, Main, NextScript } from "next/document";
-import Head from "next/head";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
